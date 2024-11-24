c5cf31c93e74c86930d5c3e87eac6def3180c039
HamidBehnam <hamid.behnam@gmail.com>
2024-11-24 14:35:10 -0800

    use custom hook even for the query fns

--- playground-react/src/components/Content/Content.tsx
+++ playground-react/src/components/Content/Content.tsx
@@ -1,11 +1,12 @@
 import {FC} from "react";
 import {useQuery} from "@tanstack/react-query";
-import {fetchMovies} from "../../services/api.service";
 import {Movie} from "../../types";
 import MovieList from "../MovieList/MovieList";
+import useMoviesApi from "../../hooks/useMoviesApi";
 
 const Content: FC = () => {
-  const {data: movies = [], error, isLoading} = useQuery<Movie[]>({queryKey: ['movies'], queryFn: fetchMovies});
+  const {getMovies} = useMoviesApi();
+  const {data: movies = [], error, isLoading} = useQuery<Movie[]>({queryKey: ['movies'], queryFn: getMovies});
 
   return (
     <div className={'flex-1'}>
--- playground-react/src/services/api.service.ts
+++
@@ -1,5 +0,0 @@
-import fetchApi from "./fetch.service";
-
-export const fetchMovies = async () => {
-  return fetchApi('/mocked_apis/movies.json');
-};
