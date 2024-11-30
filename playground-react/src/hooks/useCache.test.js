01d4523d53860198c26baad76389bcf13bcd9ddc
HamidBehnam <hamid.behnam@gmail.com>
2024-11-29 19:59:53 -0800

    fix the tests

--- playground-react/src/hooks/useCache.test.js
+++ playground-react/src/hooks/useCache.test.js
@@ -1,5 +1,5 @@
 import { renderHook } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 import useCache from "./useCache";
 
 describe("useCache", () => {
