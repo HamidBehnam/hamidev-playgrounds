94e6b51d5cd1f61309dce598b8361488fb2c597e
Hamid Behnam <hamidbehnam@Hamids-MacBook-Pro.local>
2026-07-26 18:04:13 -0700

    add the auto cleaning version of the call_model_stream module

--- first-llm-call/main.py
+++ first-llm-call/main.py
@@ -3,7 +3,7 @@ from translate import translate
 from call_model import call_model
 from chat import chat
 from anthropic.types import MessageParam
-from call_model_stream_async import call_model_stream_async
+from call_model_stream_auto_cleaning import call_model_stream_auto_cleaning
 import asyncio
 
 
@@ -21,7 +21,7 @@ async def main() -> int:
     translated = translate(word="hello", language="french")
     print(f"translation result: {translated}")
 
-    await call_model_stream_async(messages=[{
+    call_model_stream_auto_cleaning(messages=[{
         "role": "user",
         "content": "Provide the current stage of beteljoose star life. did I use the name of the star correctly?"
     }])
